6435e85c62f75de5e8bb0ceacaa7b988a9e4d430
preetpannu <preetpannu2025@gmail.com>
2024-09-28 18:29:18 +0400

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import HomeScreen from ".screens/HomeScreen";
 import DonationScreen from "./screens/DonationScreen";
 import FirstScreen from "./screens/FirstScreen";
@@ -9,24 +11,22 @@ import PointsScreen from "./screens/PointsScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import ThankYouScreen from "./screens/ThankYouScreen";
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Donation: DonationScreen,
-    LogIn: LogInScreen,
-    First: FirstScreen,
-    News: NewsScreen,
-    Points: PointsScreen,
-    Square: SquareScreen,
-    SignUp: SignUpScreen,
-    ThankYou: ThankYouScreen,
-  },
-  {
-    initialRouteName: "First",
-    defaultNavigationOptions: {
-      title: "App",
-    },
-  }
-);
+const Stack = createStackNavigator();
 
-export default createAppContainer(navigator);
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignUpScreen">
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+        <Stack.Screen name="DonationScreen" component={DonationScreen} />
+        <Stack.Screen name="ThankYouScreen" component={ThankYouScreen} />
+        <Stack.Screen name="NewsScreen" component={NewsScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="PointsScreen" component={PointsScreen} />
+        <Stack.Screen name="FirstScreen" component={FirstScreen} />
+        <Stack.Screen name="LogInScreen" component={LogInScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
